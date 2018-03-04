54ede7523e97c21d47e395ad2cf505abcd90d94b
ricardoperezf <ricardojpf98@gmail.com>
2018-03-04 11:48:35 -0600

    Modified keys in vector

--- calest/models/Medals.py
+++ calest/models/Medals.py
@@ -11,35 +11,35 @@ class Medal:
     def get_medals_discipline(self):
         array = pandas.DataFrame(self.csv_file.Discipline.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({'discipline': str(key), 'count': int(value['Discipline'])})
+            self.vector.append({'type': str(key), 'count': int(value['Discipline'])})
         return self.vector
 
     def get_medals_gender(self):
         array = pandas.DataFrame(self.csv_file.Gender.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({'gender': str(key), 'count': int(value['Gender'])})
+            self.vector.append({'type': str(key), 'count': int(value['Gender'])})
         return self.vector
 
     def get_medals_city(self):
         array = pandas.DataFrame(self.csv_file.City.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({'city': str(key), 'count': int(value['City'])})
+            self.vector.append({'type': str(key), 'count': int(value['City'])})
         return self.vector
 
     def get_medals_country(self):
         array = pandas.DataFrame(self.csv_file.NOC.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({"country": str(key), "medals": int(value['NOC'])})
+            self.vector.append({"type": str(key), "count": int(value['NOC'])})
         return self.vector
 
     def get_medals_by_medals(self):
         array = pandas.DataFrame(self.csv_file.Medal.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({'medal': str(key), 'count': int(value['Medal'])})
+            self.vector.append({'type': str(key), 'count': int(value['Medal'])})
         return self.vector
 
     def get_medals_by_sport(self):
         array = pandas.DataFrame(self.csv_file.Sport.value_counts())
         for key, value in array.iterrows():
-            self.vector.append({'sport': str(key), 'count': int(value['Sport'])})
+            self.vector.append({'type': str(key), 'count': int(value['Sport'])})
         return self.vector
