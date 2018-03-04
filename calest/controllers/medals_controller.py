88ead676750f4a44dd2f277c3d81b1ae94bc1c97
gsusfm <36603838+gsusfm@users.noreply.github.com>
2018-03-03 22:14:45 -0600

    Update medals_controller.py

--- calest/controllers/medals_controller.py
+++ calest/controllers/medals_controller.py
@@ -14,34 +14,39 @@ def post_discipline():
 
 @calest_app.route('/gender', methods=["POST"])
 def post_gender():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
     result = Medal(csv_file).get_medals_gender()
     return jsonify(result), 201
 
 
 @calest_app.route('/city', methods=["POST"])
 def post_city():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
     result = Medal(csv_file).get_medals_city()
     return jsonify(result), 201
 
 
 @calest_app.route('/country', methods=["POST"])
 def post_country():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
     result = Medal(csv_file).get_medals_country()
     return jsonify(result), 201
 
 
 @calest_app.route('/medal', methods=["POST"])
 def post_medals():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
     result = Medal(csv_file).get_medals_by_medals()
     return jsonify(result), 201
 
 
 @calest_app.route('/sport', methods=["POST"])
 def post_sport():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
     result = Medal(csv_file).get_medals_by_sport()
     return jsonify(result), 201
