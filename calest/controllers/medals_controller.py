e77df1b2543fc04705eb68220914ea2a4c4322bc
gsusfm <36603838+gsusfm@users.noreply.github.com>
2018-03-12 21:04:31 -0600

    Update medals_controller.py

--- calest/controllers/medals_controller.py
+++ calest/controllers/medals_controller.py
@@ -27,6 +27,7 @@ def post_discipline():
     })
     return jsonify("Added"), 201
 
+#obtiene todo
 
 @calest_app.route('/result', methods=["GET"])
 def get_discipline():
@@ -40,6 +41,84 @@ def get_discipline():
             'medals': medals["medal"],
             'city': medals["city"],
             'country': medals["country"],
+            'gender': medals["gender"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por deporte
+
+@calest_app.route('/sport', methods=["GET"])
+def get_discipline_sport():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'sport': medals["sport"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por disciplina
+
+@calest_app.route('/discipline', methods=["GET"])
+def get_discipline_discipline():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'discipline': medals["discipline"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por medallas
+
+@calest_app.route('/medal', methods=["GET"])
+def get_discipline_medal():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'medal': medals["medal"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por ciudad
+
+@calest_app.route('/city', methods=["GET"])
+def get_discipline_city():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'city': medals["city"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por pais
+
+@calest_app.route('/country', methods=["GET"])
+def get_discipline_country():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'country': medals["country"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por genero
+
+@calest_app.route('/gender', methods=["GET"])
+def get_discipline_gender():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
             'gender': medals["gender"]
         })
     return jsonify(vector)
