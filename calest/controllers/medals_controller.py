45e0a47effb44d21949957293d41bbaaa9ba4768
gsusfm <36603838+gsusfm@users.noreply.github.com>
2018-03-13 19:43:00 -0600

    Update medals_controller.py

--- calest/controllers/medals_controller.py
+++ calest/controllers/medals_controller.py
@@ -1,46 +1,124 @@
 import pandas
-from calest import calest_app
+from calest import calest_app, client
 from flask import request, jsonify
 from ..models.Medals import Medal
 
+db = client.estadisticas
+collection = db.medallas
 
-@calest_app.route('/discipline', methods=["POST"])
+
+@calest_app.route('/result', methods=["POST"])
 def post_discipline():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_discipline()
-    return jsonify(result), 201
+    csv_received = request.files['file']
+    csv_file = pandas.read_csv(csv_received)
+    discipline_result = Medal(csv_file).get_medals_discipline()
+    gender_result = Medal(csv_file).get_medals_gender()
+    city_result = Medal(csv_file).get_medals_city()
+    country_result = Medal(csv_file).get_medals_country()
+    medals_result = Medal(csv_file).get_medals_by_medals()
+    sport_result = Medal(csv_file).get_medals_by_sport()
+    cursor = collection.insert({
+        "discipline": discipline_result,
+        "gender": gender_result,
+        "city": city_result,
+        "country": country_result,
+        "medal": medals_result,
+        "sport": sport_result
+    })
+    return jsonify("Added"), 201
 
+#obtiene todo
 
-@calest_app.route('/gender', methods=["POST"])
-def post_gender():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_gender()
-    return jsonify(result), 201
+@calest_app.route('/result', methods=["GET"])
+def get_discipline():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'sport': medals["sport"],
+            'discipline': medals["discipline"],
+            'medals': medals["medal"],
+            'city': medals["city"],
+            'country': medals["country"],
+            'gender': medals["gender"]
+        })
+    return jsonify(vector)
 
+#obtiene medallas por deporte
 
-@calest_app.route('/city', methods=["POST"])
-def post_city():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_city()
-    return jsonify(result), 201
+@calest_app.route('/sport', methods=["GET"])
+def get_discipline_sport():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'sport': medals["sport"]
+        })
+    return jsonify(vector)
 
+#obtiene medallas por disciplina
 
-@calest_app.route('/country', methods=["POST"])
-def post_country():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_country()
-    return jsonify(result), 201
+@calest_app.route('/discipline', methods=["GET"])
+def get_discipline_discipline():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'discipline': medals["discipline"]
+        })
+    return jsonify(vector)
 
+#obtiene medallas por medallas
 
-@calest_app.route('/medal', methods=["POST"])
-def post_medals():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_by_medals()
-    return jsonify(result), 201
+@calest_app.route('/medal', methods=["GET"])
+def get_discipline_medal():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'medal': medals["medal"]
+        })
+    return jsonify(vector)
 
+#obtiene medallas por ciudad
 
-@calest_app.route('/sport', methods=["POST"])
-def post_sport():
-    csv_file = pandas.read_csv('/home/virtualizacion/CalestAPIr/CalestAPI/calest/data/medals.csv')
-    result = Medal(csv_file).get_medals_by_sport()
-    return jsonify(result), 201
+@calest_app.route('/city', methods=["GET"])
+def get_discipline_city():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'city': medals["city"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por pais
+
+@calest_app.route('/country', methods=["GET"])
+def get_discipline_country():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'country': medals["country"]
+        })
+    return jsonify(vector)
+
+#obtiene medallas por genero
+
+@calest_app.route('/gender', methods=["GET"])
+def get_discipline_gender():
+    cursor = collection.find()
+    vector = []
+    for medals in cursor:
+        print(medals)
+        vector.append({
+            'gender': medals["gender"]
+        })
+    return jsonify(vector)
