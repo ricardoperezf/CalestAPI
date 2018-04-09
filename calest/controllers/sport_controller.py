887b58211a36124ad4b1254222f4f7c0987b2095
ricardoperezf <ricardojpf98@gmail.com>
2018-04-08 18:28:14 -0600

    Added sport controller

--- calest/controllers/sport_controller.py
+++ calest/controllers/sport_controller.py
@@ -1,3 +1,10 @@
+import pandas
+from calest import calest_app, client
+from flask import request, jsonify
+from ..models.Medals import Medal
+
+db = client.estadisticas
+collection = db.medallas
 
 
 # obtiene medallas por deporte
@@ -10,75 +17,5 @@ def get_discipline_sport():
         print(medals)
         vector.append({
             'sport': medals["sport"]
-        })
-    return jsonify(vector)
-
-
-# obtiene medallas por disciplina
-
-@calest_app.route('/discipline', methods=["GET"])
-def get_discipline_discipline():
-    cursor = collection.find()
-    vector = []
-    for medals in cursor:
-        print(medals)
-        vector.append({
-            'discipline': medals["discipline"]
-        })
-    return jsonify(vector)
-
-
-# obtiene medallas por medallas
-
-@calest_app.route('/medal', methods=["GET"])
-def get_discipline_medal():
-    cursor = collection.find()
-    vector = []
-    for medals in cursor:
-        print(medals)
-        vector.append({
-            'medal': medals["medal"]
-        })
-    return jsonify(vector)
-
-
-# obtiene medallas por ciudad
-
-@calest_app.route('/city', methods=["GET"])
-def get_discipline_city():
-    cursor = collection.find()
-    vector = []
-    for medals in cursor:
-        print(medals)
-        vector.append({
-            'city': medals["city"]
-        })
-    return jsonify(vector)
-
-
-# obtiene medallas por pais
-
-@calest_app.route('/country', methods=["GET"])
-def get_discipline_country():
-    cursor = collection.find()
-    vector = []
-    for medals in cursor:
-        print(medals)
-        vector.append({
-            'country': medals["country"]
-        })
-    return jsonify(vector)
-
-
-# obtiene medallas por genero
-
-@calest_app.route('/gender', methods=["GET"])
-def get_discipline_gender():
-    cursor = collection.find()
-    vector = []
-    for medals in cursor:
-        print(medals)
-        vector.append({
-            'gender': medals["gender"]
         })
     return jsonify(vector)
